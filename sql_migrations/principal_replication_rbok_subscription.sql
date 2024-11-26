8ddf08d9fbf0d1c2743113c3868a03b53fc5521d
Alexander Gorbuntsov <goralex02@yandex.ru>
2024-11-26 19:10:24 +0300

    минор апдейт

--- sql_migrations/principal_replication_rbok_subscription.sql
+++ sql_migrations/principal_replication_rbok_subscription.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 CREATE SUBSCRIPTION clients_subscription_branch1
 CONNECTION 'host=branch1 port=5432 dbname=branch1_db user=principal_user password=principal_password'
 PUBLICATION clients_publication;
@@ -5,3 +7,5 @@ PUBLICATION clients_publication;
 CREATE SUBSCRIPTION clients_subscription_branch2
 CONNECTION 'host=branch2 port=5432 dbname=branch2_db user=principal_user password=principal_password'
 PUBLICATION clients_publication;
+
+COMMIT;

0d0dac9a2b41d183665d5be4a0db0b5d74dfaea7
Alexander Gorbuntsov <goralex02@yandex.ru>
2024-11-26 18:45:06 +0300

    Добавлена репликация клиентов в центр

---
+++ sql_migrations/principal_replication_rbok_subscription.sql
@@ -0,0 +1,7 @@
+CREATE SUBSCRIPTION clients_subscription_branch1
+CONNECTION 'host=branch1 port=5432 dbname=branch1_db user=principal_user password=principal_password'
+PUBLICATION clients_publication;
+
+CREATE SUBSCRIPTION clients_subscription_branch2
+CONNECTION 'host=branch2 port=5432 dbname=branch2_db user=principal_user password=principal_password'
+PUBLICATION clients_publication;

0a5af8856b50bcedc59626c26890cdcf9183ab4a
Alexander Gorbuntsov <goralex02@yandex.ru>
2024-11-27 18:00:33 +0300

    no slots

--- sql_migrations/branch_1_replication_rbok_subscription.sql
+++ sql_migrations/branch_1_replication_rbok_subscription.sql
@@ -1,7 +1,5 @@
 CREATE SUBSCRIPTION clients_subscription_branch_1_to_branch_2
 CONNECTION 'host=branch_2 port=5432 dbname=branch_2_db user=branch_1_user password=branch_1_password'
 PUBLICATION clients_publication_branch_2
-WITH (copy_data = false, create_slot = false, slot_name = 'custom_slot_name');
-ALTER SUBSCRIPTION clients_subscription_branch_1_to_branch_2 SET (synchronous_commit = 'remote_apply');
-
-
+WITH (copy_data = false)--, create_slot = false, slot_name = 'custom_slot_name');
+--ALTER SUBSCRIPTION clients_subscription_branch_1_to_branch_2 SET (synchronous_commit = 'remote_apply');
--- sql_migrations/branch_2_replication_rbok_subscription.sql
+++ sql_migrations/branch_2_replication_rbok_subscription.sql
@@ -1,7 +1,5 @@
 CREATE SUBSCRIPTION clients_subscription_branch_2_to_branch_1
 CONNECTION 'host=branch_1 port=5432 dbname=branch_1_db user=branch_2_user password=branch_2_password'
 PUBLICATION clients_publication_branch_1
-WITH (copy_data = false, create_slot = false, slot_name = 'custom_slot_name');
-ALTER SUBSCRIPTION clients_subscription_branch_2_to_branch_1 SET (synchronous_commit = 'remote_apply');
-
-
+WITH (copy_data = false)--, create_slot = false, slot_name = 'custom_slot_name');
+--ALTER SUBSCRIPTION clients_subscription_branch_2_to_branch_1 SET (synchronous_commit = 'remote_apply');
